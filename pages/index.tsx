128ff45f91c37dd3831a3fc8998bcda085d664d2
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-08-28 17:22:22 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,51 @@
 import Layout from "../components/layout"
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import { useRouter } from 'next/router'; // Import useRouter for programmatic navigation
 
-export default function IndexPage() {
+export default function Home() {
+  const router = useRouter();
+
+  // Function to navigate to the specified page
+  const navigateToPage = (page) => {
+    router.push(page);
+  };
+
   return (
-    <Layout>
-      <h1>NextAuth.js Example</h1>
-      <p>
-        This is an example site to demonstrate how to use{" "}
-        <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
-      </p>
-    </Layout>
+    <div className="container">
+      <Head>
+        <title>FitPlanPro</title>
+        <link rel="icon" href="/FitPlanPro.png" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to Your Fitness App!" />
+        <p className="description">
+          Get started by exploring the following features:
+        </p>
+
+        <div className="feature-grid">
+          <div className="feature-row">
+            <button onClick={() => navigateToPage('/goal-setting')}>Goal Setting</button>
+            <button onClick={() => navigateToPage('/nutrition-tips')}>Nutrition and Hydration Tips</button>
+            <button onClick={() => navigateToPage('/protected')}>Protected</button>
+
+          </div>
+          <div className="feature-row">
+            <button onClick={() => navigateToPage('/ExerciseLibrary')}>Exercise Library</button>
+            <button onClick={() => navigateToPage('/custom-workouts')}>Custom Workouts</button>
+          </div>
+          <div className="feature-row">
+            <button onClick={() => navigateToPage('/pages/profile')}>profile</button>
+            <button onClick={() => navigateToPage('/reminders')}>Reminder and Scheduling</button> 
+          </div>
+
+        </div>
+      </main>
+
+      <Footer />
+    </div>
   )
 }
+

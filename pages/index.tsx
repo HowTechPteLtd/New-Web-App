a395136da108b8e776a1cc699705c3d3a00389f7
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-08-28 17:45:08 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
-import Header from '../components/header';
-import Footer from '../components/footer';
-import { useRouter } from 'next/router'; // Import useRouter for programmatic navigation
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const router = useRouter();
@@ -19,29 +19,40 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to Your Fitness App!" />
+        <Header />
         <p className="description">
           Get started by exploring the following features:
         </p>
 
         <div className="feature-grid">
           <div className="feature-row">
-            <button onClick={() => navigateToPage('/goal-setting')}>Goal Setting</button>
-            <button onClick={() => navigateToPage('/nutrition-tips')}>Nutrition and Hydration Tips</button>
+            <button onClick={() => navigateToPage('/goal-setting')}>
+              Goal Setting
+            </button>
+            <button onClick={() => navigateToPage('/nutrition-tips')}>
+              Nutrition and Hydration Tips
+            </button>
             <button onClick={() => navigateToPage('/protected')}>Protected</button>
           </div>
           <div className="feature-row">
-            <button onClick={() => navigateToPage('/ExerciseLibrary')}>Exercise Library</button>
-            <button onClick={() => navigateToPage('/custom-workouts')}>Custom Workouts</button>
+            <button onClick={() => navigateToPage('/ExerciseLibrary')}>
+              Exercise Library
+            </button>
+            <button onClick={() => navigateToPage('/custom-workouts')}>
+              Custom Workouts
+            </button>
           </div>
           <div className="feature-row">
-            <button onClick={() => navigateToPage('/pages/profile')}>profile</button>
-            <button onClick={() => navigateToPage('/reminders')}>Reminder and Scheduling</button> 
+            <button onClick={() => navigateToPage('/pages/profile')}>Profile</button>
+            <button onClick={() => navigateToPage('/reminders')}>
+              Reminder and Scheduling
+            </button>
           </div>
         </div>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
+
